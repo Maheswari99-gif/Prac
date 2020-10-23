53beb1bc4a5e996fa120313f6e5eb0ef80b13f09
Maheswari99-gif <maheswarikoyyani999@gmail.com>
2020-10-23 11:29:49 +0530

    Mahi

---
+++ src/main/java/com/capgemini/javaprac/App.java
@@ -0,0 +1,13 @@
+package com.capgemini.javaprac;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
